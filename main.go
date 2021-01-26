e4df319d3c6defc71c1b92b0ce9d9875e71a5e5a
evanezcent <pratamayo16@gmail.com>
2021-01-26 15:11:26 +0700

    add middleware authorization

--- main.go
+++ main.go
@@ -2,20 +2,38 @@ package main
 
 import (
 	"Atlantis-Backend/config"
+	"Atlantis-Backend/controller"
+	"Atlantis-Backend/repository"
+	"Atlantis-Backend/service"
 	"fmt"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+)
+
+var (
+	db             *gorm.DB                  = config.InitConnection()
+	userRepository repository.UserRepository = repository.NewUserRepository(db)
+	jwtService     service.JWTService        = service.NewJwtService()
+	userService    service.UserService       = service.NewUserService(userRepository)
+	userController controller.UserController = controller.NewUserController(jwtService, userService)
 )
 
 func main() {
 	fmt.Println("Starting apps...")
-	config.InitConnection()
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+
+	authRoutes := r.Group("api/v1/user")
+	{
+		authRoutes.POST("/login", userController.Login)
+		authRoutes.POST("/register", userController.Register)
+	}
+
+	userRoutes := r.Group("api/v1/user", middleware.AuthorizeJWT(jwtService))
+	{
+		userRoutes.GET("/get", userController.Get)
+		userRoutes.PUT("/update", userController.Update)
+	}
 
 	r.Run()
 }
---
+++ middleware/jwt-auth.go
@@ -0,0 +1,35 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"Atlantis-Backend/helper"
+	"Atlantis-Backend/service"
+
+	"github.com/dgrijalva/jwt-go"
+	"github.com/gin-gonic/gin"
+)
+
+// AuthorizeJWT to authorize jwt in api
+func AuthorizeJWT(jwtServ service.JWTService) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			res := helper.ResponseFailed("Failed to process request", "Token invalid", nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, res)
+			return
+		}
+
+		token, err := jwtServ.ValidateToken(authHeader)
+		if token.Valid {
+			claims := token.Claims.(jwt.MapClaims)
+			log.Println("Claims[userID]: ", claims["userID"])
+			log.Println("Claims[issuer]: ", claims["issuer"])
+		} else {
+			log.Println(err)
+			response := helper.ResponseFailed("Token invalid", err.Error(), nil)
+			ctx.AbortWithStatusJSON(http.StatusUnauthorized, response)
+		}
+	}
+}
