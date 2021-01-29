cc38c82d5da585a9fbaa195f465938ffc7cf1e9f
evanezcent <pratamayo16@gmail.com>
2021-01-29 13:21:54 +0700

    create item dto and change type in item model

---
+++ dto/item.go
@@ -0,0 +1,19 @@
+package dto
+
+// ItemCreateDTO is used to catch body json from client
+type ItemCreateDTO struct {
+	Title         string `json:"title" form:"title" binding:"required"`
+	SpesificDate  string `form:"spesific_date" json:"spesific_date" binding:"required"`
+	SpesificPlace string `form:"spesific_place" json:"spesific_place" binding:"required"`
+	Description   string `json:"description" form:"description" binding:"required"`
+	UserID        string `json:"user_id" form:"user_id" binding:"required"`
+	IsDone        bool   `json:"is_done" form:"is_done"`
+}
+
+// ItemUpdateDTO is used to catch body json from client
+type ItemUpdateDTO struct {
+	Title         string `json:"title" form:"title" binding:"required"`
+	SpesificDate  string `form:"spesific_date" json:"spesific_date" binding:"required"`
+	SpesificPlace string `form:"spesific_place" json:"spesific_place" binding:"required"`
+	Description   string `json:"description" form:"description" binding:"required"`
+}
--- models/item.go
+++ models/item.go
@@ -1,17 +1,14 @@
 package models
 
-import (
-	"time"
-)
-
 // Item model for database
 type Item struct {
-	ID             uint64    `gorm:"primary_key;auto_increment" json:"id"`
-	Title          string    `gorm:"type:varchar(255)" json:"title"`
-	DateLostFound  time.Time `gorm:"not null" json:"date_lostfound"`
-	PlaceLostFound string    `gorm:"type:text" json:"place_lostfound"`
-	Description    string    `gorm:"type:text" json:"description"`
-	UserID         string    `gorm:"not null" json:"-"`
-	User           User      `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
-	DeletedAt      time.Time `gorm:"null" json:"deleted_at"`
+	ID            uint64 `gorm:"primary_key;auto_increment" json:"id"`
+	Title         string `gorm:"type:varchar(255)" json:"title"`
+	SpesificDate  string `gorm:"not null" json:"spesific_date"`
+	SpesificPlace string `gorm:"type:text" json:"spesific_place"`
+	Description   string `gorm:"type:text" json:"description"`
+	UserID        string `gorm:"not null" json:"-"`
+	User          User   `gorm:"foreignkey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
+	IsDone        bool   `gorm:"default:false" json:"is_done"`
+	DeletedAt     string `gorm:"null" json:"deleted_at"`
 }
